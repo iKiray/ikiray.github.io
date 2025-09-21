653cde51d612255ad685c340f38ab8b69abb93b1
Sky <exkiray@gmail.com>
2025-09-21 06:56:31 -0400

    changed show hide button to a +/- button that changes accordingly

--- js/projects.js
+++ js/projects.js
@@ -1,17 +1,23 @@
 function p1showhide() {
-    let x = document.getElementById("p1");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    let b = document.getElementById("b1");
+    let p = document.getElementById("p1");
+    if (p.style.display === "none") {
+        b.innerText="-"
+        p.style.display = "block";
     } else {
-        x.style.display = "none";
+        b.innerText="+"
+        p.style.display = "none";
     }
 }
 
 function p2showhide() {
-    let x = document.getElementById("p2");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    let b = document.getElementById("b2");
+    let p = document.getElementById("p2");
+    if (p.style.display === "none") {
+        b.innerText="-"
+        p.style.display = "block";
     } else {
-        x.style.display = "none";
+        b.innerText="+"
+        p.style.display = "none";
     }
 }
