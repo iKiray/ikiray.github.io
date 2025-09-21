28dfc9d323876edcc91bf6626acc13836e2b5999
Sky <sky@I5-6-940.home>
2025-09-21 06:45:47 -0400

    added projects page js functions for showing and hiding content, with css styling for the button

---
+++ js/projects.js
@@ -0,0 +1,17 @@
+function p1showhide() {
+    let x = document.getElementById("p1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function p2showhide() {
+    let x = document.getElementById("p2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
